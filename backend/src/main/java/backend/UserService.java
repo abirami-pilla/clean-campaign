de1dc558df67dbeb21330a2010124f5ae5386894
abirami-pilla <abirami2k17@gmail.com>
2025-09-10 00:22:38 +0530

    Register and list users

---
+++ backend/src/main/java/backend/UserService.java
@@ -0,0 +1,25 @@
+package backend;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already registered!");
+        }
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
+
